36c9cf3ab9f2a39cb3059aabe37860826abd1b92
Fanie Oosthuysen <fanie.oosthuysen@gmail.com>
2018-06-14 22:36:55 +0200

    preventStatusBarExpansion

--- src/android/KioskActivity.java
+++ src/android/KioskActivity.java
@@ -13,6 +13,9 @@ public class KioskActivity extends CordovaActivity {
     public static volatile boolean running = false;
     public static volatile boolean kioskModeEnabled = false;
 
+    public Context context = null;
+    private static final boolean IS_AT_LEAST_LOLLIPOP = Build.VERSION.SDK_INT >= 21;
+
     protected void onStart() {
         super.onStart();
         System.out.println("KioskActivity started");
@@ -28,14 +31,65 @@ public class KioskActivity extends CordovaActivity {
     public void onCreate(Bundle savedInstanceState) {
         System.out.println("KioskActivity paused");
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY);
         super.init();
 
         if (running) {
             finish(); // prevent more instances of kiosk activity
         }
 
+        context = IS_AT_LEAST_LOLLIPOP ? cordova.getActivity().getWindow().getContext() : cordova.getActivity().getApplicationContext();
+
         loadUrl(launchUrl);
+        preventStatusBarExpansion(context);
+    }
+    
+    public static void preventStatusBarExpansion(Context context) {
+        WindowManager manager = ((WindowManager) context.getApplicationContext()
+            .getSystemService(Context.WINDOW_SERVICE));
+    
+        Activity activity = (Activity)context;
+        WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
+        localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+        localLayoutParams.gravity = Gravity.TOP;
+        localLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|
+    
+        // this is to enable the notification to recieve touch events
+        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+    
+        // Draws over status bar
+        WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+    
+        localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        int resId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        int result = 0;
+        if (resId > 0) {
+            result = activity.getResources().getDimensionPixelSize(resId);
+        }
+    
+        localLayoutParams.height = result;
+    
+        localLayoutParams.format = PixelFormat.TRANSPARENT;
+    
+        customViewGroup view = new customViewGroup(context);
+    
+        manager.addView(view, localLayoutParams);
+    }
+    
+    public static class customViewGroup extends ViewGroup {
+    
+        public customViewGroup(Context context) {
+            super(context);
+        }
+    
+        @Override
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        }
+    
+        @Override
+        public boolean onInterceptTouchEvent(MotionEvent ev) {
+            Log.v("customViewGroup", "**********Intercepted");
+            return true;
+        }
     }
 
     @Override
